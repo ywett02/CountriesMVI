0dcadf40980393272625175783e077ccfb1c51e7
ivet <iveta.jurcikova@strv.com>
2018-09-24 22:07:41 +0200

    Add filter menu

--- app/src/main/java/com/jurcikova/ivet/countries/mvi/ui/MainActivity.kt
+++ app/src/main/java/com/jurcikova/ivet/countries/mvi/ui/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.jurcikova.ivet.countries.mvi.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -17,10 +19,23 @@ class MainActivity : AppCompatActivity() {
 
         binding.toolbar.let {
             it.title = title
-            it.inflateMenu(R.menu.menu_search)
-            it.setOnMenuItemClickListener {item ->
-                NavigationUI.onNavDestinationSelected(item, Navigation.findNavController(this, R.id.my_nav_host_fragment))
+            it.inflateMenu(R.menu.menu_main)
+            it.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.countrySearchFragment -> NavigationUI.onNavDestinationSelected(item, Navigation.findNavController(this, R.id.my_nav_host_fragment))
+                    R.id.menu_filter -> {
+                        showFilteringPopUpMenu(item)
+                    }
+                    else -> false
+                }
             }
         }
     }
+
+    private fun showFilteringPopUpMenu(item: MenuItem): Boolean =
+            PopupMenu(this, findViewById(R.id.menu_filter)).let { menu ->
+                menu.menuInflater.inflate(R.menu.menu_filter, menu.menu)
+                menu.show()
+                true
+            }
 }
